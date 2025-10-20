7c957e07767799d80dcd77d756c518851bb411e7
陳仕軒 <chenshixuan@chenshixuandeMac-mini.local>
2025-10-21 01:52:49 +0800

    neon db

--- PerformerApi/Program.cs
+++ PerformerApi/Program.cs
@@ -1,44 +1,34 @@
-using PerformerApi.Data;
 using Microsoft.EntityFrameworkCore;
-
+using PerformerApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
+var connStr = Environment.GetEnvironmentVariable("DATABASE_URL")
+              ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
-// 加入 PostgreSQL EF
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(connStr, npgsql =>
+    {
+        npgsql.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+    }));
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// 可在開發/生產皆啟用 Swagger（方便 demo）
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// 啟動時自動套用 migration（會建立缺少的 table）
+//using (var scope = app.Services.CreateScope())
+//{
+    //var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    //db.Database.Migrate();
+//}
 
 app.UseHttpsRedirection();
-app.UseCors("AllowFrontend");
-app.UseAuthorization();
-app.MapControllers();               
+app.MapControllers();
 app.Run();
